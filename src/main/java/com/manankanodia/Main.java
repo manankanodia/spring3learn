e60fdf00c60a8ae81b4d470da77ba30d9c586429
manankanodia <manankanodia@gmail.com>
2023-07-23 09:50:43 +0530

    Implemented Read API List all

--- src/main/java/com/manankanodia/Main.java
+++ src/main/java/com/manankanodia/Main.java
@@ -3,13 +3,22 @@ package com.manankanodia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -30,5 +39,11 @@ public class Main {
             List<String> favLanguages,
             Person person
     ){}
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+//        return List.of();
+        return customerRepository.findAll();
+    }
 
 }
