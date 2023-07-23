26e5c3ad10ec3ca3f18691bc365fcbb7a6d2a0ba
manankanodia <manankanodia@gmail.com>
2023-07-23 10:04:43 +0530

    Implemented Create addCustomer API

--- src/main/java/com/manankanodia/Main.java
+++ src/main/java/com/manankanodia/Main.java
@@ -2,9 +2,7 @@ package com.manankanodia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +42,17 @@ public class Main {
     public List<Customer> getCustomers() {
 //        return List.of();
         return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(String name, String email, Integer age) {}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setAge(request.age);
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customerRepository.save(customer);
     }
 
 }
